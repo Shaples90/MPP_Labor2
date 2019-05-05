e08021af880ee49c110f482b98f9f42a9a6d3799
Shaples90 <daejinseon@hotmail.com>
2019-05-05 17:26:17 +0200

    Erstes Commit für MPP_Versuch1_Aufgabe1

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "tm4c1294ncpdt.h"
 
-int main(int argc, char const *argv[])
+void wait(unsigned long time)
 {
-      printf("Hello World\n");
-      printf("Wir sollten mit Git arbeiten\n");
-      return 0;
+   for (unsigned long i = 0; i < time; i++);
+}
+
+void main(int argc, char const *argv[])
+{
+   unsigned char old_input, new_input = 0x00;               // local input signal storage
+
+   SYSCTL_RCGCGPIO_R |= ((1 << 10) | (1 << 11));            // clock enable port L & M
+   while((SYSCTL_PRGPIO_R & ((1 << 10) | (1 << 11))) == 0); // control peripheral ready bits
+   GPIO_PORTL_DEN_R = 0x01;                                 // digital I/O pin PL(0)
+   GPIO_PORTM_DEN_R = 0xFF;                                 // digital I/O pins PM(0:7)
+   GPIO_PORTL_DIR_R = 0x00;                                 // define input direction for PL(0)
+   GPIO_PORTM_DIR_R = 0xFF;                                 // define output direction for PM(0:7)
+
+   while(1)
+   {
+      new_input = GPIO_PORTL_DATA_R;                        // read new input signal
+      if(old_input != new_input)                            // compare old with new input signal
+      {
+         GPIO_PORTM_DATA_R |= 0xFF;                         // high output signal to PM(0:7)
+         wait(100);
+      }
+      else
+      {
+         GPIO_PORTM_DATA_R &= 0x00;                         // low output signal to PM(0:7)
+         wait(100);
+      }
+      old_input = new_input;                                // store new to old input signal
+   }
 }
