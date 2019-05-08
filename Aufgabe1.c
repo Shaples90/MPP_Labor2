12c992626bda384c962bdbf44c73cbcd52e6ded9
Shaples90 <daejinseon@hotmail.com>
2019-05-08 21:33:42 +0200

    Laborversuch 2 Aufgabe 1 funktioniert

--- Aufgabe1.c
+++ Aufgabe1.c
@@ -6,7 +6,8 @@
 
 void wait(unsigned long time)
 {
-   for (unsigned long i = 0; i < time; i++);                // wait-loop
+   unsigned long i;
+   for (i = 0; i < time; i++);                              // wait-loop
 }
 
 void main(int argc, char const *argv[])
@@ -26,12 +27,12 @@ void main(int argc, char const *argv[])
       if(old_input != new_input)                            // compare old with new input signal
       {
          GPIO_PORTM_DATA_R |= 0xFF;                         // high output signal to PM(0:7)
-         wait(100);                                         // wait output signal until it reachess the LEDs
+         wait(5000);                                        // wait output signal until it reachess the LEDs
       }
       else
       {
          GPIO_PORTM_DATA_R &= 0x00;                         // low output signal to PM(0:7)
-         wait(100);                                         // wait output signals until they arrive
+         wait(5000);                                        // wait output signals until they arrive
       }
       old_input = new_input;                                // store new signal to old input signal
    }
