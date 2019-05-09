8f1c30a74dbe1217fe83d9706f7637d931d1faa8
Shaples90 <daejinseon@hotmail.com>
2019-05-09 14:24:49 +0200

    Laborversuch 2

--- Aufgabe2.c
+++ Aufgabe2.c
@@ -5,18 +5,27 @@
 #include <stdint.h>
 #include "tm4c1294ncpdt.h"
 
+void wait(unsigned long time)
+{
+   unsigned long i;
+   for (i = 0; i < time; i++);             // wait-loop
+}
+
 void main(int argc, char const *argv[])
 {
-   unsigned short measureDistance;         // local measureDistance storage
+   unsigned short timeMicroSeconds, timeMilliSeconds, measureDistance;
 
-   // configure PD(0)
-   SYSCTL_RCGCGPIO_R |= (1 << 3);         // clock enable port D
-   while(!(SYSCTL_PRGPIO_R & (1 << 3)));  // wait for port P clock
-   GPIO_PORTD_DEN_R |= (1 << 0);          // PD(0) enable
-   GPIO_PORTD_AFSEL_R |= (1 << 0);        // PD(0) alternate function
-   GPIO_PORTD_PCTL_R |= 0x03;             // PD(0) connected to Timer0A
+   // configure PD(0) and PD(1)
+   SYSCTL_RCGCGPIO_R |= (1 << 3);            // clock enable port D
+   while(!(SYSCTL_PRGPIO_R & (1 << 3)));     // wait for port D clock
+   GPIO_PORTD_DEN_R |= (1 << 0) | (1 << 1);  // PD(0) and PD(1) enable
+   GPIO_PORTD_AFSEL_R |= (1 << 0);           // PD(0) alternate function
+   GPIO_PORTD_PCTL_R |= 0x03;                // PD(0) connected to Timer0A
+   GPIO_PORTD_DIR_R |= 0x02;                 // PD(1) define output
+   GPIO_PORTD_DATA_R |= 0x02;                // PD(1) to High
 
    // configure Timer 0
+
    SYSCTL_RCGCTIMER_R |= (1 << 0);        // clock enable Timer0
    while(!(SYSCTL_PRTIMER_R & (1 << 0))); // wait for Timer0 clock
    TIMER0_CTL_R &= ~0x01;                 // disable Timer0 for config
@@ -30,12 +39,16 @@ void main(int argc, char const *argv[])
    while (1)
    {
       // synchronize to next edge
-      while((TIMER0_RIS_R & (1 << 2)) == 0);                   // wait for capture event
-      TIMER0_ICR_R |= (1 << 2);                                // clear Timer0a capture event flag
-      TIMER0_CTL_R |= 0x01;                                    // re-enable Timer0A
-      while((TIMER0_RIS_R & (1 << 2)) == 0);                   // wait for capture event
-      measureDistance = ((((unsigned short) (0xFFFF - TIMER0_TAR_R)) / 16) * 0.001) * 34.3;         // calculate measureDistance in cm
-      printf("measure distance is: %d\n", measureDistance);   // print measureDistance on console
-      TIMER0_ICR_R |= (1 << 2);                                // clear capture event flag
+      GPIO_PORTD_DATA_R &= 0x00;                                           // PD(1) to Low
+      while((TIMER0_RIS_R & (1 << 2)) == 0);                               // wait for capture event
+      TIMER0_ICR_R |= (1 << 2);                                            // clear Timer0a capture event flag
+      TIMER0_CTL_R |= 0x01;                                                // re-enable Timer0A
+      while((TIMER0_RIS_R & (1 << 2)) == 0);                               // wait for capture event
+      timeMicroSeconds = ((unsigned short) (0xFFFF - TIMER0_TAR_R)) / 16;  // measured time in micro seconds
+      timeMilliSeconds = timeMicroSeconds * 0.001;                         // measured time in milli seconds
+      measureDistance = timeMilliSeconds * 34,4;                           // measured Distance in cm
+      printf("measure distance is: %d\n", measureDistance);                // print measureDistance on console
+      TIMER0_ICR_R |= (1 << 2);                                            // clear capture event flag
+      GPIO_PORTD_DATA_R |= 0x02;                                           // PD(1) to High
    }
 }
